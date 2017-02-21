b826500b14822f761a0ff278a63e4f0fe9c9b7fd
SviridovSV <sviridov.sv82@gmail.com>
2017-02-21 21:34:11 +0200

    finish project_controller, add view for home page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,54 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index]
+
+  def show
+  end
+
+  def index
+    @projects = current_user ? current_user.projects.order(id: :desc) : []
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.' 
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    if current_user
+      @projects = current_user.projects.order(id: :desc)
+    else
+      @projects = []
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
