94ba9345529f8e6c39357c7e98f5fa612830f1f5
SviridovSV <sviridov.sv82@gmail.com>
2017-03-03 12:25:28 +0200

    create views for index and new

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,27 +2,40 @@ class ProjectsController < ApplicationController
   before_action :set_project, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index]
 
-  def show
-  end
-
   def index
-    @projects = current_user ? current_user.projects.order(id: :desc) : []
+    @projects ||= current_user.projects.order(id: :desc) 
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def new
     @project = Project.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
-    @project = Project.new(project_params)
-    if @project.save
-      redirect_to @project, notice: 'Project was successfully created.' 
-    else
-      render action: 'new'
+    @project = Project.create(project_params)
+    @project.user = current_user
+    respond_to do |format |
+      if @project.save
+        flash[:success] = 'Project was successfully created.'
+        format.js 
+      else
+        format.js { render :new }
+      end
     end
   end
 
   def edit
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def update
@@ -31,14 +44,17 @@ class ProjectsController < ApplicationController
     else
       render action: 'edit'
     end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
     @project.destroy
-    if current_user
-      @projects = current_user.projects.order(id: :desc)
-    else
-      @projects = []
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
