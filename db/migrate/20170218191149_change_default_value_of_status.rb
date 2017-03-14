526e83ca04232af2d1e302ec2a5e86bfc629d8c0
SviridovSV <sviridov.sv82@gmail.com>
2017-03-14 21:46:34 +0200

    new database

--- db/migrate/20170218191149_change_default_value_of_status.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultValueOfStatus < ActiveRecord::Migration
-  def change
-    change_column_default :tasks, :status, to: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218191149) do
+ActiveRecord::Schema.define(version: 20170218185539) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170218191149) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
